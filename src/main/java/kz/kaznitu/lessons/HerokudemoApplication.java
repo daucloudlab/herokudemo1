5953bcc36048ea3aadfabc3a8590d2dcf7ce4f62
Daulet Baimbetov <daucloudlab@gmail.com>
2018-04-03 10:24:50 +0600

    version 1

---
+++ src/main/java/kz/kaznitu/lessons/HerokudemoApplication.java
@@ -0,0 +1,14 @@
+package kz.kaznitu.lessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class HerokudemoApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokudemoApplication.class, args);
+	}
+}
---
+++ src/main/java/kz/kaznitu/lessons/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package kz.kaznitu.lessons.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/test")
+    public String home(){
+        return "home" ;
+    }
+}
